54d48ca5931977f0f46e4145b337844639a6e954
Nobie <nobahar.ziaei@ipricegroup.com>
2017-03-06 14:54:31 +0800

    added contact me link in the home pgae

---
+++ client/resources/views/contact-me.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+  <div id="contact-me">
+   Team name is has been stablished on 6 march 2017 in Kuala Lumpur.
+  </div>
+@stop
--- client/routes/web.php
+++ client/routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/readme', function () {
     return view('readme');
 });
+
+Route::get('/contact-me', function () {
+    return view('contact-me');
+});
