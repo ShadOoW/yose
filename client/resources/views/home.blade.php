44c6585d5b598dd65f9bddc88e6013b7fcd61ce8
AdelBachene <adel.bachene@trustedcompany.com>
2017-03-06 15:49:44 +0800

    fixing homepage link

--- client/resources/views/home.blade.php
+++ client/resources/views/home.blade.php
@@ -8,6 +8,7 @@
   <div class="links">
     <a href="https://iprice.my">Team Name</a>
     <a href="https://github.com/ShadOoW/yose">GitHub</a>
+    <a href="{{ url('minesweeper') }}">Minesweeper</a>
     <a id="contact-me-link" href="{{ url('contact-me') }}">Contact Us</a>
     <a id="repository-link" href="{{ url('readme') }}">Readme</a>
     <a id="ping-challenge-link" href="{{ config('app.api') }}/ping">Portfolio - Ping</a>
