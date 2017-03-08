da01e29edcf7d8412e904de568ce7b4a01dabe77
visser300 <ngocquang19877@gmail.com>
2017-03-08 16:01:45 +0700

    fix bugs for non multiple entry case

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -16,29 +16,25 @@ class Controller extends BaseController
         foreach($data as $entry) {
             $args = explode("=", $entry);
             $value = $args[1];
-            $results[] = $this->getPrimeData($value);                
-        }
-
-        return response()->json($results);
-    }
-
-    public function getPrimeData($value)
-    {
-        $num = intval($value);
-        if (is_numeric($value) && strlen($value) < 7) {
-        $primes = array();
-            for ($candidate = 2; $num > 1; $candidate++) {
-                for (; $num % $candidate == 0; $num /= $candidate) {
-                    $primes[] = $candidate;
+            
+            $num = intval($value);
+                if (is_numeric($value) && strlen($value) < 7) {
+                    $primes = array();
+                    for ($candidate = 2; $num > 1; $candidate++) {
+                        for (; $num % $candidate == 0; $num /= $candidate) {
+                            $primes[] = $candidate;
+                        }
+                    }
+                    $results[] = ['number' => intval($value), 'decomposition' => $primes];
+                } else {
+                    if (strlen($value) < 7) {
+                        $results[] = ['number' => $value, 'error' => 'not a number'];
+                    } else {
+                        $results[] = ['number' => $value, 'error' => 'too big number (>1e6)'];
+                    }
                 }
-            }
-            return ['number' => intval($value), 'decomposition' => $primes];
-        } else {
-            if (strlen($value) < 7) {
-                return ['number' => $value, 'error' => 'not a number'];
-            } else {
-                return ['number' => $value, 'error' => 'too big number (>1e6)'];
-            }
         }
+
+        return response()->json(count($results) === 1 ? $results[0] : $results);
     }
 }
