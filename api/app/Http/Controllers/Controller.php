9db3d6dd3381aa4288ea3a70cdaf6576c4787ca4
visser300 <ngocquang19877@gmail.com>
2017-03-08 15:49:50 +0700

    Update API for multiple entries

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -9,26 +9,36 @@ class Controller extends BaseController
 {
     public function calculate(Request $request)
     {
-        $num = intval($request["number"]);
-        if (is_numeric($request["number"]) && strlen($request["number"]) < 7) {
-            $primes = array();
+        $data = $_SERVER['QUERY_STRING'];
+        $data = explode("&", $data);
+
+        $results = [];
+        foreach($data as $entry) {
+            $args = explode("=", $entry);
+            $value = $args[1];
+            $results[] = $this->getPrimeData($value);                
+        }
+
+        return response()->json($results);
+    }
+
+    public function getPrimeData($value)
+    {
+        $num = intval($value);
+        if (is_numeric($value) && strlen($value) < 7) {
+        $primes = array();
             for ($candidate = 2; $num > 1; $candidate++) {
                 for (; $num % $candidate == 0; $num /= $candidate) {
                     $primes[] = $candidate;
                 }
             }
-            return response()
-                ->json(['number' => intval($request["number"]), 'decomposition' => $primes]);
-
+            return ['number' => intval($value), 'decomposition' => $primes];
         } else {
-            if (strlen($request["number"]) < 7) {
-                return response()
-                    ->json(['number' => $request["number"], 'error' => 'not a number']);
+            if (strlen($value) < 7) {
+                return ['number' => $value, 'error' => 'not a number'];
             } else {
-                return response()
-                    ->json(['number' => $request["number"], 'error' => 'too big number (>1e6)']);
+                return ['number' => $value, 'error' => 'too big number (>1e6)'];
             }
         }
-
     }
 }
